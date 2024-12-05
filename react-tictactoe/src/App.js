f43d4040836b9199894f8555008b1ca42e6e4c61
chkim <kimch3617@naver.com>
2024-12-05 16:30:30 +0900

    Update: react tictactoe 샘플 앱 클론 코딩

--- react-tictactoe/src/App.js
+++ react-tictactoe/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Board } from './Board';
 
 function App() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(false);
+  const currentSquares = history[history.length - 1];
+
+  const onPlay = (newSquares) => {
+    setHistory([...history, newSquares]);
+    setXIsNext(prev => !prev);
+  }
+
+  const jumpTo = (index) => {
+    const jumpToHistory = history.slice(0, index + 1);
+
+    setHistory(jumpToHistory);
+    setXIsNext(jumpToHistory.length % 2 === 0);
+  }
+
+  const moves = history.map((step, index) => {
+    const desc = index ?
+      `Go to move #${index}` :
+      `Go to game start`;
+
+    return (
+      <li className='list-item-jump'>
+        <button className='btn-jump' onClick={() => jumpTo(index)}>{desc}</button>
+      </li>
+    )
+  })
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  const handleClick = (i) => {
+    if (calculateWinner(currentSquares) || currentSquares[i]) {
+      return;
+    }
+
+    const newSquares = currentSquares.slice();
+
+    if (xIsNext) {
+      newSquares[i] = 'X';
+    } else {
+      newSquares[i] = 'O';
+    }
+
+    onPlay(newSquares);
+  }
+
+  const winner = calculateWinner(currentSquares);
+  let status;
+  
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquares} xIsNext={xIsNext} onClick={handleClick} />
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
---
+++ react-tictactoe/src/Board.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Square } from './Square'
+import './Board.css'
+
+export const Board = ({ squares, xIsNext, onClick }) => {
+    return (
+        <div>
+            <div className="board-row">
+                <Square value={squares[0]} onClick={() => onClick(0)} />
+                <Square value={squares[1]} onClick={() => onClick(1)} />
+                <Square value={squares[2]} onClick={() => onClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onClick={() => onClick(3)} />
+                <Square value={squares[4]} onClick={() => onClick(4)} />
+                <Square value={squares[5]} onClick={() => onClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onClick={() => onClick(6)} />
+                <Square value={squares[7]} onClick={() => onClick(7)} />
+                <Square value={squares[8]} onClick={() => onClick(8)} />
+            </div>
+        </div>
+    )
+}
